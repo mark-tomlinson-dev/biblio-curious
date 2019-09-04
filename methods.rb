226f6845248b6860338ad4369bb2107cc8b5959c
Mark Tomlinson <mark.tomlinson3@gmail.com>
2019-09-04 15:46:21 +1000

    refactored menu method

--- methods.rb
+++ methods.rb
@@ -1,26 +1,53 @@
-def main_menu
-	puts "Enter an option"
-	selection = gets.chomp.to_i
-	case (selection)
-	when 1
-		puts "Option 1"
-		true
+# def main_menu
+# 	puts "Welcome to Biblio Curious! What would you like to do today?"
+# 	puts "\n"
+# 	puts "1. Exercise my curiosity" 
+# 	puts "2. View my saved curiosities"
+# 	puts "3. Exit"
+# 	print "> "
+# 	selection = gets.chomp.to_i
+	
+# 	case (selection)
+# 	when 1
+# 		puts "What are you feeling curious about?"
+# 		print "> "
+# 		search_term = gets.strip
+# 			if search_term == nil
+# 				puts "That is not a valid entry"
+# 			end 
+# 		true
+# 	when 2
+# 		puts "Here are your saved books: #{bookshelf}"
+# 		true
+# 	when 10
+# 		puts "Thanks for stopping by"
+# 		# exit
+# 		false
+# 	end
+# end
+
+# continue_menu = true
+
+# while continue_menu
+#  continue_menu = main_menu
+# end
+
+
+
+def menu
+	puts "Pick a number between 1 and 3"
+	puts "1. Prints hi"
+	puts "2. Runs method again"
+	puts "3. Exits the file"
+	print "> "
+	input = gets.chomp.to_i
+	case input 
+	when 1 
+		puts "hi"
+		menu
 	when 2
-		puts "Option 2"
-		true
-	when 10
-		puts "10"
-		false
-	end
-end
-
-
-continue_menu = true
-while continue_menu
- continue_menu = menu
-end
-
-
-def boo
-
-
+		menu
+	when 3 
+		exit
+	end  
+end 
