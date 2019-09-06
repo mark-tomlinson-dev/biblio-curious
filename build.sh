0e38fd6a02fc3aee2473dc4df58b0650c57e4aec
Mark Tomlinson <mark.tomlinson3@gmail.com>
2019-09-06 12:22:21 +1000

    updated .sh file so that it will run

--- build.sh
+++ build.sh
@@ -1,11 +1,7 @@
-# allows you to execute ./build.sh without the bash keyword
-chmod +x ./build.sh
-# copy across files to distribution directory
 mkdir dist
-cp ./app.rb ./dist
-cp ./contacts.rb ./dist
-cp ./test.rb ./dist
+cp ./main.rb ./dist
+cp ./methods.rb ./dist
 # install all gems
 gem install googlebooks
 gem install colorize 
-gem install 
+gem install tty-font
