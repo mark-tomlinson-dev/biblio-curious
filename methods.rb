884969c38b75a7dc314ff0e0b72927fd837064bf
Mark Tomlinson <mark.tomlinson3@gmail.com>
2019-09-06 11:18:17 +1000

    added aasci art, colorize, and other aesthetic and user experience improvements

--- methods.rb
+++ methods.rb
@@ -1,9 +1,13 @@
+
+
+
 def google_books_api_search
 	puts "\n"
 	puts "What are you feeling curious about?"
 	puts "\n"
 	print "> "
 	search_term = gets.strip
+	system "clear"
 	if search_term == nil
 		puts "That is not a valid entry. Please try again"
 	end
@@ -35,18 +39,19 @@ def what_would_you_like_to_do_next
 	puts "3. Save to my bookshelf"
 	puts "4. Conduct a new search"
 	puts "5. Take me back to the main menu"
+	puts "\n"
+	print "> "
 end 
 
 def choose_a_book(books)
-	puts "\n"
 	print "> "
-	puts "\n"
 	book_index = gets.chomp.to_i - 1
+	`clear`
 	return books.to_a[book_index]
 end 
 
 def search_again_or_go_back_to_menu(bookshelf)
-	sleep 2
+	sleep 1
 	puts "\n"
 	puts "Do any of these titles interest you?"
 	puts "\n"
@@ -55,11 +60,14 @@ def search_again_or_go_back_to_menu(bookshelf)
 	puts "3. Take me back to the main menu"
 	puts "\n"
 	print "> "
-	response = gets.chomp.to_i 
+	response = gets.chomp.to_i
+	puts "\n"
 	if response == 1
-		puts "\n"
 		puts "Which title number?"
+		puts "\n"
+		puts "\n"
 	elsif response == 2
+		system 'clear'
 		search(bookshelf)
 	else response == 3
 		menu(bookshelf)
@@ -80,23 +88,25 @@ def search(bookshelf)
 	search_again_or_go_back_to_menu(bookshelf)
 	# choose a book 
 	book_chosen = choose_a_book(books)
-	sleep 2
+	sleep 2 
 	puts "\n"
-	puts "Here is a bit of information about it:"
+	puts "Cool. Here is a bit of information about it:"
 	puts "\n"
-	puts "Title: #{book_chosen.title}"
+	puts "Title:".colorize(:magenta) + " #{book_chosen.title}"
+	
 	puts "\n"
-	puts "Author(s): #{book_chosen.authors}"
+	puts "Author(s):".colorize(:magenta) + " #{book_chosen.authors}"
 	if book_chosen.authors == nil
 		puts "Oops there is no author information available for this book!"
 	end 
 	puts "\n"
-	puts "Description: #{book_chosen.description}"
+	puts "Description:".colorize(:magenta) + " #{book_chosen.description}"
 	if book_chosen.description == nil 
 		puts "Oops there is no description available for this book!"
 	end
 	puts "\n"
-	puts "Publication Date: #{book_chosen.published_date}"
+	puts "Publication Date:".colorize(:magenta) + " #{book_chosen.published_date}"
+	puts "\n"
 	if book_chosen.published_date == nil 
 		puts "Oops there is no publication date available for this book!"
 	end
@@ -108,22 +118,23 @@ def search(bookshelf)
 		selection = gets.chomp.to_i 
 		if selection == 1
 			puts "\n"
-			puts "Click here for preview (command + double click): #{book_chosen.preview_link}"
+			puts "Click here for preview " + "(command + double click):".colorize(:magenta) + " #{book_chosen.preview_link}"
+			puts "\n"
 		elsif selection == 2
 			puts "\n"
-			puts "Click here to view cover (command + double click): #{book_chosen.image_link(:zoom => 4)}"
+			puts "Click here to view cover " + "(command + double click):".colorize(:magenta) + " #{book_chosen.image_link(:zoom => 4)}"
+			puts "\n"
 		elsif selection == 3
 			add_to_bookshelf(book_chosen.title, bookshelf)
 			# bookshelf << book_chosen.title 
 			puts "\n"
 			puts "Your bookshelf has been updated!"
 			puts "\n"
-			sleep 5
+			sleep 2
 		elsif selection == 4
 			search(bookshelf)
 		else selection == 5
 			submenu_running = false
-			sleep 3 
 		end 
 	end 
 	menu(bookshelf)
@@ -138,32 +149,59 @@ end
 		else 
 			puts "Your bookshelf is currently empty"
 		end  
-		sleep 3 
+		sleep 2 
 		menu(bookshelf)
 end 
 
-# def welcome_message
-# end 
-
-
 def menu(bookshelf)
-	puts "\n"
-	puts "Welcome to Biblio Curious! What would you like to do today?"
-	puts "\n"
-	puts "1. Exercise my curiosity" 
-	puts "2. View my saved curiosities"
-	puts "3. Exit"
-	puts "\n"
-	print "> "
-	selection = gets.chomp.to_i
+	puts '''
+	  ___________________________________________________________
+											
+
+	'''
+	puts FONT.write("Biblio Curious")
+	puts '''
+
+	  ____________________________________________________________
+	'''
+	sleep 2
+
 	
+			puts '''
+			Welcome to Biblio-Curious!
+			
+			What would you like to do today?
+			1. Exercise my curiosity
+			2. View my saved curiosities
+			3. Exit
+			'''
+		
+	selection = gets.chomp.to_i
+	system "clear"
 	case selection 
 	when 1 
 		search(bookshelf)
 	when 2
 		display_bookshelf(bookshelf)
 	when 3
-		puts "Thanks for dropping by. See you again soon!" 
+		puts """
+
+    _____
+   /    /|_ ___________________________________________
+  /    // /|                                          /|
+ (====|/ //   Thanks for dropping by...              / |
+  (=====|/     See you again soon!   
+ (====|/                                           / /||
+/_________________________________________________/ / ||
+|  _____________________________________________  ||  ||
+| ||                                            | ||
+| ||                                            | ||
+| |                                             | |  
+
+------------------------------------------------
+
+
+ """ 
 		exit
 	else 
 		puts "\n"
@@ -172,4 +210,4 @@ def menu(bookshelf)
     sleep 2
 		menu(bookshelf)
 	end  
-end
+end 
